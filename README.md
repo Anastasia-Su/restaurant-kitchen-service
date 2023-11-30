0a23691fdaca5260b5e504a89946e8b78fc21edc
Anastasia <anastasiiya.p@gmail.com>
2023-11-30 13:33:38 +0200

    Change settings for deploy

---
+++ README.md
@@ -0,0 +1,22 @@
+![Logo of the project](https://raw.githubusercontent.com/jehna/readme-best-practices/master/sample-logo.png)
+
+# Kitchen Service
+> Django project for managing cooks, dishes and dish types in Kitchen.
+
+
+
+## Installing / Getting started
+
+```shell
+git clone https://github.com/Anastasia-Su/restaurant-kitchen-service
+cd restaurant-kitchen-service
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+## Features
+
+* Authentication functionality for cooks (users)
+* Managing dishes and dish types directly from website interface
+* Admin panel for advanced managing
--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 import dotenv
 from django.core.management.utils import get_random_secret_key
@@ -24,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 dotenv.load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -54,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -95,6 +98,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
+
 
 
 # Password validation
