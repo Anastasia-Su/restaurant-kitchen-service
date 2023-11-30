107069ac64facd44aef24c74fa33784449ba8278
Anastasia <anastasiiya.p@gmail.com>
2023-11-30 16:13:24 +0200

    Change DEBUG in settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -28,8 +28,7 @@ dotenv.load_dotenv()
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
